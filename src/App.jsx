8b135796453493bcc28795d077e3538eb44d07ad
Harsh <imharshalashi333@gmail.com>
2023-03-28 20:08:18 +0530

    done with api part

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,48 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Container, Box } from '@chakra-ui/react';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import TextInput from './components/TextInput';
 
 const App = () => {
+  const [keywords, setKeywords] = useState('');
+  const [isOpen, setIsOpen] = useState(false);
+  const [loading, setLoading] = useState(false);
+
+  const extractKeywords = async (text) => {
+    setIsOpen(true);
+    setLoading(true);
+
+    const options = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${import.meta.env.VITE_OPENAI_API_KEY}`,
+      },
+      body: JSON.stringify({
+        model: 'text-davinci-003',
+        prompt:
+          'Extract Keywords from this text. Make the first letter of each word uppercase and separate them with commas.\n\n' +
+          text +
+          '',
+        temperature: 0.5,
+        max_tokens: 60,
+        frequency_penalty: 0.8,
+      }),
+    };
+    const response = await fetch(import.meta.env.VITE_OPENAI_API_URL, options);
+    const json = await response.json();
+    const data = json.choices[0].text.trim();
+    console.log(data);
+    setKeywords(data);
+    setLoading(false);
+  };
+
   return (
     <Box bg="blue.600" color="white" height="100vh" paddingTop={130}>
       <Container maxW="3xl" centerContent>
         <Header>My App</Header>
-        <TextInput />
+        <TextInput extractKeywords={extractKeywords} />
         <Footer></Footer>
       </Container>
     </Box>
--- src/components/TextInput.jsx
+++ src/components/TextInput.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { useState } from 'react';
 import { Textarea, Button, useToast } from '@chakra-ui/react';
 
-const TextInput = () => {
+const TextInput = ({ extractKeywords }) => {
   const [text, setText] = useState('');
   const toast = useToast();
 
@@ -16,7 +16,7 @@ const TextInput = () => {
         duration: 3000,
       });
     } else {
-      console.log(text);
+      extractKeywords(text);
     }
   };
   return (
